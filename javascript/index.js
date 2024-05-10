a90194ddaff9af8e9910aa2582af73bff2a1975a
Yujiro Akihiro <yujiro.akihiro@gmail.com>
2024-05-11 01:36:56 +0200

    #2

--- javascript/index.js
+++ javascript/index.js
@@ -7,7 +7,7 @@ getInstruction('mashedPotatoes', 0, (step0) => {
       document.querySelector("#mashedPotatoes").innerHTML += `<li>${step2}</li>`;
       getInstruction('mashedPotatoes', 3, (step3) => {
         document.querySelector("#mashedPotatoes").innerHTML += `<li>${step3}</li>`;
-        document.querySelector("#mashedPotatoes").innerHTML += `<li>Mashed potatoes are ready!</li>`;
+        document.querySelector("#mashedPotatoes").innerHTML += `<li class="ready">Mashed potatoes are ready!</li>`;
       }, (error) => {
         console.error(error);
       });
@@ -49,45 +49,45 @@ obtainInstruction('steak', 0)
   })
   .then((step6) => {
     document.querySelector("#steak").innerHTML += `<li>${step6}</li>`;
-    document.querySelector("#steak").innerHTML += `<li>Steak is ready!</li>`;
+    document.querySelector("#steak").innerHTML += `<li class="ready">Steak is ready!</li>`;
   })
   .catch((error) => {
     console.error(error);
   });
 
 
-  async function makeBroccoli() {
-    try {
-  
-      // Obtain instructions for Broccoli
-      const steps = [
-        await obtainInstruction('broccoli', 0),
-        await obtainInstruction('broccoli', 1),
-        await obtainInstruction('broccoli', 2),
-        await obtainInstruction('broccoli', 3),
-        await obtainInstruction('broccoli', 4),
-        await obtainInstruction('broccoli', 5),
-        await obtainInstruction('broccoli', 6)
-      ];
-  
-      // Add each step to the list
-      steps.forEach(step => {
-        document.querySelector("#broccoli").innerHTML += `<li>${step}</li>`;
-      });
-  
-      // Add final message
-      document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready!</li>`;
-  
-      // Display the broccoli image
-      document.querySelector("#broccoliImg").removeAttribute("hidden");
-    } catch (error) {
-      console.error('Error:', error);
-    }
+async function makeBroccoli() {
+  try {
+
+    // Obtain instructions for Broccoli
+    const steps = [
+      await obtainInstruction('broccoli', 0),
+      await obtainInstruction('broccoli', 1),
+      await obtainInstruction('broccoli', 2),
+      await obtainInstruction('broccoli', 3),
+      await obtainInstruction('broccoli', 4),
+      await obtainInstruction('broccoli', 5),
+      await obtainInstruction('broccoli', 6)
+    ];
+
+    // Add each step to the list
+    steps.forEach(step => {
+      document.querySelector("#broccoli").innerHTML += `<li>${step}</li>`;
+    });
+
+    // Add final message
+    document.querySelector("#broccoli").innerHTML += `<li>Broccoli is ready!</li>`;
+
+    // Display the broccoli image
+    document.querySelector("#broccoliImg").removeAttribute("hidden");
+  } catch (error) {
+    console.error('Error:', error);
   }
-  
-  makeBroccoli();
-  
-  
+}
+
+makeBroccoli();
+
+
 
 
 // Bonus 1
@@ -105,7 +105,7 @@ allStepsCompleted.then(() => {
   document.querySelector("#broccoliImg").removeAttribute("hidden");
 });
 
-// Bonus 2
+// Bonus: Iteration 5
 const brusselsSproutsSteps = [];
 for (let i = 0; i < brusselsSprouts.length - 1; i++) {
   brusselsSproutsSteps.push(obtainInstruction('brusselsSprouts', i));
@@ -120,5 +120,5 @@ Promise.all(brusselsSproutsSteps)
     brusselsSproutsList.innerHTML += `<li>Brussels sprouts are ready!</li>`;
   })
   .catch(error => {
-    console.error('Error:', error);
+    console.error('Promise.all error:', error);
   });
